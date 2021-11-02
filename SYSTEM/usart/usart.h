1c75d6ba5da17a48c040ed14aa940ea679ebaa5c
wonderinsolitude <74749629+wonderinsolitude@users.noreply.github.com>
2021-11-02 16:45:06 +0800

    Add files via upload

---
+++ SYSTEM/usart/usart.h
@@ -0,0 +1,28 @@
+/***********************************************
+公司：东莞市微宏智能科技有限公司
+品牌：WHEELTEC
+官网：wheeltec.net
+淘宝店铺：shop114407458.taobao.com 
+速卖通: https://minibalance.aliexpress.com/store/4455017
+版本：5.7
+修改时间：2021-04-29
+
+Company: WeiHong Co.Ltd
+Brand: WHEELTEC
+Website: wheeltec.net
+Taobao shop: shop114407458.taobao.com 
+Aliexpress: https://minibalance.aliexpress.com/store/4455017
+Version:5.7
+Update：2021-04-29
+
+All rights reserved
+***********************************************/
+#ifndef __USART_H
+#define __USART_H
+#include "stdio.h"	
+#include "sys.h" 
+
+void uart_init(u32 bound);
+#endif
+
+
